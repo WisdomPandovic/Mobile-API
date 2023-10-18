59e8c27fd3e50ceceb931b5654f719baa923363b
pandovic <wisdompandovic@gmail.com>
2023-10-19 00:42:29 +0100

    updated

--- routes/app.js
+++ routes/app.js
@@ -6,4 +6,12 @@ require('./routes/post')(app)
 require('./routes/tag')(app)
 // require('./routes/comment')(app)
 
+// Middleware to handle default routes
+app.use((req, res, next) => {
+    if (req.originalUrl === '/' || req.originalUrl === '/favicon.ico') {
+      return res.status(204).end(); // Return a non-content response
+    }
+    next(); // Continue to the next middleware
+  });
+
 module.exports = app  
